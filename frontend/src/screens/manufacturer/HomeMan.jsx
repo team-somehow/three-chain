6622ba0d2183094875076e9341b9d6b0d689d0f4
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-04-15 19:00:59 +0530

    feat: add map

--- frontend/src/screens/manufacturer/HomeMan.jsx
+++ frontend/src/screens/manufacturer/HomeMan.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Map, Marker } from "react-map-gl";
 
 function HomeMan() {
   return (
     <div>
-        <h1>Map</h1>
+      <Map
+        initialViewState={mapInitialViewState}
+        style={mapContainerStyles}
+        mapStyle="mapbox://styles/mbmph/clghxs0u2006401pkcfls6cog"
+        mapboxAccessToken={
+          "pk.eyJ1IjoibWJtcGgiLCJhIjoiY2tya2F0OTJvMGk1YjJwbGZ1bDJ1eGU0dCJ9.fLJp01SsIpdhGmWdBzaSnQ"
+        }
+        renderWorldCopies={false}
+        onClick={() => {}}
+      >
+        <Marker
+          latitude={19.100343}
+          longitude={72.898441}
+          style={markerStyle}
+          onClick={() => {}}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "/map/pin1.png"}
+            width={30}
+            alt="da"
+          />
+        </Marker>
+      </Map>
     </div>
   );
 }
+
+// initial state on the map
+const mapInitialViewState = {
+  latitude: 20.5937,
+  longitude: 78.9629,
+  zoom: 5,
+};
+
+export const mapContainerStyles = {
+  height: "100vh",
+  width: "100vw",
+};
+
+const markerStyle = {
+  cursor: "pointer",
+};
 
 export default HomeMan;

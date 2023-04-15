135fdeff739041602ed2ee0ce3eccc5b62e2ced8
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-04-15 14:47:31 +0530

    feat: update productNFT contract

--- hardhat/contracts/ProductNFT.sol
+++ hardhat/contracts/ProductNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "erc721a/contracts/ERC721A.sol";
+import "hardhat/console.sol";
 
 contract ProductNFT is ERC721A{
     constructor() ERC721A("ThreeChain","THC"){}
@@ -19,6 +20,15 @@ contract ProductNFT is ERC721A{
     mapping(uint256=>bool) escrowProcess;
     mapping(uint256=>address) escrowRecevier;
     mapping(uint256 =>address) escrowSender;
+
+
+    struct Bid {
+        address bidder;
+        uint256 bidPrice;
+        bool isWinner;
+    }
+     // to maintain downpayment
+    mapping(uint256 => Bid[]) public batchIdToBid;
     
     function batchMint(uint256 quantity,string memory name,uint256 batchUid) public payable {
         uint256 start=_nextTokenId();
@@ -40,17 +50,39 @@ contract ProductNFT is ERC721A{
         batchData[batchId].verification=true;
     }
 
-    function escrowStart(uint256 batchId) public payable  {
-        require(batchData[batchId].verification,"Need to be verified!");
-        batchData[batchId].currentPrice=msg.value;
+    // function escrowStart(uint256 batchId) public payable  {
+    //     require(batchData[batchId].verification,"Need to be verified!");
+    //     batchData[batchId].currentPrice=msg.value;
+    //     escrowProcess[batchId]=true;
+    //     escrowRecevier[batchId]=msg.sender;
+    // }
+
+    function escrowBid(uint256 batchId) public payable {
+        batchIdToBid[batchId].push(Bid(msg.sender, msg.value, false));
         escrowProcess[batchId]=true;
-        escrowRecevier[batchId]=msg.sender;
     }
-    function escrowBatch(uint256 batchId) public {
+
+    function escrowBatch(uint256 batchId, address winnerAddress) public {
         require(escrowProcess[batchId] ,"Need the escrow process to start!");
-        escrowSender[batchId]=batchData[batchId].owner;
-        batchData[batchId].owner=address(this);
+
+        escrowSender[batchId] = batchData[batchId].owner;
+        batchData[batchId].owner = address(this);
+
+        // loop over all bids and set the winner
+        for (uint i = 0; i < batchIdToBid[batchId].length; i++) {
+            if (batchIdToBid[batchId][i].bidder == winnerAddress) {
+                batchIdToBid[batchId][i].isWinner = true;
+                // console.log("Winner is: %s", winnerAddress);
+                escrowRecevier[batchId]=winnerAddress;
+            } else {
+                // refund the losers
+                payable(batchIdToBid[batchId][i].bidder)
+                    .transfer(batchIdToBid[batchId][i].bidPrice);
+                // console.log("Loser is: %s", batchIdToBid[batchId][i].bidder);
+            }
+        }
     }
+
     function escrowEnd(uint256 batchId) public  {
         Batch memory data= batchData[batchId];
         payable(escrowSender[batchId]).transfer(data.currentPrice);
@@ -59,6 +91,7 @@ contract ProductNFT is ERC721A{
             safeTransferFrom(escrowSender[batchId],escrowRecevier[batchId],i);
         }
     }
+    
     function getOwner(uint256 batchId) public view returns (address){
         return batchData[batchId].owner;
     }

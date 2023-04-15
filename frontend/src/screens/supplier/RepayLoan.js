a4ac0175186494c953e36664ab0f7170bc058791
Killjoy <aryaajitnair@gmail.com>
2023-04-15 17:59:58 +0530

    feat: Add arcana in repay Loan

--- frontend/src/screens/supplier/RepayLoan.js
+++ frontend/src/screens/supplier/RepayLoan.js
@@ -4,57 +4,67 @@ import { db } from "../../config/firebase";
 import { useNavigate, useParams } from "react-router-dom";
 import { Box, Button, Typography } from "@mui/material";
 import RuppeInput from "../../components/RuppeInput";
+import { providers, Contract, ethers } from "ethers";
+import { arcanaProvider } from "../..";
+import { LoanContractAddress } from "../../constants/constants";
+import LoanAbi from "../../artifacts/contracts/Loan.sol/Loan.json";
 
 function RepayLoan() {
-  const loanId = useParams().id;
-  const [loanDetails, setLoanDetails] = useState({
-    name: "",
-    amount: "",
-    interest: "",
-    totalAmount: "",
-  });
-  const [repaymentAmount, setRepaymentAmount] = useState(0);
-  const navigate = useNavigate();
-  const getData = async () => {
-    const loanData = await getDoc(doc(db, "RequestedLoans", loanId))
-      .then((doc) => doc.data())
-      .then((data) => {
-        setLoanDetails(data);
-      });
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-  const onSubmit = async () => {
-    const totalAmount = loanDetails.totalAmount - repaymentAmount;
-
-    await updateDoc(
-      doc(db, "RequestedLoans", loanId),
-      totalAmount <= 0
-        ? {
-            loanStatus: "Paid",
-          }
-        : {
-            totalAmount: loanDetails.totalAmount - repaymentAmount,
-          }
-    );
-    getData();
-    navigate("/supplier/seeLoans");
-  };
-  return (
-    <Box m={4}>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <Typography>Name: {loanDetails.name} </Typography>
-        <Typography>Interest: {loanDetails.interest}% </Typography>
-        <Typography>Loan Amount: {loanDetails.amount} </Typography>
-        <Typography>Loan Amount: {loanDetails.totalAmount} </Typography>
-        <RuppeInput input={repaymentAmount} setInput={setRepaymentAmount} />
-        <Button type="submit" variant="contained" onClick={onSubmit} fullWidth>
-          Pay
-        </Button>
-      </form>
-    </Box>
-  );
+	const provider = new providers.Web3Provider(arcanaProvider.provider);
+	const signer = provider.getSigner();
+	const contract = new Contract(LoanContractAddress, LoanAbi.abi, signer);
+	const loanId = useParams().id;
+	const [loanDetails, setLoanDetails] = useState({
+		name: "",
+		amount: "",
+		interest: "",
+		totalAmount: "",
+	});
+	const [repaymentAmount, setRepaymentAmount] = useState(0);
+	const navigate = useNavigate();
+	const getData = async () => {
+		const loanData = await getDoc(doc(db, "RequestedLoans", loanId))
+			.then((doc) => doc.data())
+			.then((data) => {
+				setLoanDetails(data);
+			});
+	};
+	useEffect(() => {
+		getData();
+	}, []);
+	const onSubmit = async () => {
+		const totalAmount = loanDetails.totalAmount - repaymentAmount;
+		await arcanaProvider.connect();
+		await contract.repayLoan({
+			value: ethers.utils.parseEther(repaymentAmount.toString()),
+		});
+		await updateDoc(
+			doc(db, "RequestedLoans", loanId),
+			totalAmount <= 0
+				? {
+						loanStatus: "Paid",
+				  }
+				: {
+						totalAmount: loanDetails.totalAmount - repaymentAmount,
+				  }
+		);
+		getData();
+		navigate("/supplier/seeLoans");
+	};
+	return (
+		<Box m={4}>
+			<form onSubmit={(e) => e.preventDefault()}>
+				<Typography>Name: {loanDetails.name} </Typography>
+				<Typography>Interest: {loanDetails.interest}% </Typography>
+				<Typography>Loan Amount: {loanDetails.amount} </Typography>
+				<Typography>Loan Amount: {loanDetails.totalAmount} </Typography>
+				<RuppeInput input={repaymentAmount} setInput={setRepaymentAmount} />
+				<Button type="submit" variant="contained" onClick={onSubmit} fullWidth>
+					Pay
+				</Button>
+			</form>
+		</Box>
+	);
 }
 
 export default RepayLoan;

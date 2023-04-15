c85368abfebfa67b9688133f70a8647b41ea4bb8
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-04-16 03:35:43 +0530

    feat: dashboard nav

--- frontend/src/components/DashboardNavbar.jsx
+++ frontend/src/components/DashboardNavbar.jsx
@@ -13,79 +13,88 @@ import AddIcon from "@mui/icons-material/Add";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const DashboardNavbar = (props) => {
-  const navigate = useNavigate();
-  const currentRoute = useLocation().pathname;
-  return (
-    <Drawer
-      sx={{
-        width: 280,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: 280,
-          boxSizing: "border-box",
-          // background: "rgba(252, 254, 254, 0.43)",
-          background: "#F4F7F7",
-          // backdropFilter: "blur(25px)",
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-      classes={{ paper: "awesome-bg-0" }}
-    >
-      <Box role="presentation" p={2}>
-        <List>
-          <div
-            style={{
-              // paddingLeft: "18px",
-              marginTop: "-12px",
-              marginBottom: "28px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-
+    const navigate = useNavigate();
+    const currentRoute = useLocation().pathname;
+    return (
+        <Drawer
+            sx={{
+                width: 280,
+                flexShrink: 0,
+                "& .MuiDrawer-paper": {
+                    width: 280,
+                    boxSizing: "border-box",
+                    // background: "rgba(252, 254, 254, 0.43)",
+                    background: "#F4F7F7",
+                    // backdropFilter: "blur(25px)",
+                },
             }}
-          >
-            <ListItem onClick={() => navigate("/manufacturer/dash")}>
-              <ListItemText>
-                <h1 className="text-[#1f4e5f] font-black text-2xl">Manufacturer Dashboard</h1>
-              </ListItemText>
-            </ListItem>
-          </div>
+            variant="permanent"
+            anchor="left"
+            classes={{ paper: "awesome-bg-0" }}
+        >
+            <Box role="presentation" p={2}>
+                <List>
+                    <div
+                        style={{
+                            // paddingLeft: "18px",
+                            marginTop: "-12px",
+                            marginBottom: "28px",
+                            cursor: "pointer",
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <ListItem
+                            onClick={() => navigate("/manufacturer/dash")}
+                        >
+                            <ListItemText>
+                                <h1 className="text-[#1f4e5f] font-black text-2xl">
+                                    Manufacturer Dashboard
+                                </h1>
+                            </ListItemText>
+                        </ListItem>
+                    </div>
 
-          <NavLink
-            text={"Select Supplier"}
-            icon={<HandshakeIcon />}
-            onClickNavigateTo="/manufacturer/dash/selectSupplier"
-            isActive={currentRoute === "/manufacturer/dash/selectSupplier"}
-          />
-          <NavLink
-            text={"Approve Loans"}
-            icon={<ApartmentIcon />}
-            onClickNavigateTo="/manufacturer/dash/approveLoan"
-            isActive={currentRoute === "/manufacturer/dash/approveLoan"}
-          />
-          <NavLink
-            text={"My Batches"}
-            icon={<ChatIcon />}
-            onClickNavigateTo="/manufacturer/dash/batches"
-            isActive={currentRoute === "/manufacturer/dash/batches"}
-          />
-          <NavLink
-            text={"Create Batch"}
-            icon={<AddIcon />}
-            onClickNavigateTo="/manufacturer/dash/createBatch"
-            isActive={currentRoute === "/manufacturer/dash/createBatch"}
-          />
-        </List>
-        <Box
-          position={"absolute"}
-          width={"calc(100% - 20px)"}
-          bottom={"20px"}
-          margin={"auto"}
-        ></Box>
-      </Box>
-    </Drawer>
-  );
+                    <NavLink
+                        text={"Approve Bid"}
+                        icon={<HandshakeIcon />}
+                        onClickNavigateTo="/manufacturer/dash/bidApproval"
+                        isActive={
+                            currentRoute === "/manufacturer/dash/bidApproval"
+                        }
+                    />
+                    <NavLink
+                        text={"Approve Loans"}
+                        icon={<ApartmentIcon />}
+                        onClickNavigateTo="/manufacturer/dash/approveLoan"
+                        isActive={
+                            currentRoute === "/manufacturer/dash/approveLoan"
+                        }
+                    />
+                    <NavLink
+                        text={"My Batches"}
+                        icon={<ChatIcon />}
+                        onClickNavigateTo="/manufacturer/dash/batches"
+                        isActive={currentRoute === "/manufacturer/dash/batches"}
+                    />
+                    <NavLink
+                        text={"Create Batch"}
+                        icon={<AddIcon />}
+                        onClickNavigateTo="/manufacturer/dash/createBatch"
+                        isActive={
+                            currentRoute === "/manufacturer/dash/createBatch"
+                        }
+                    />
+                </List>
+                <Box
+                    position={"absolute"}
+                    width={"calc(100% - 20px)"}
+                    bottom={"20px"}
+                    margin={"auto"}
+                ></Box>
+            </Box>
+        </Drawer>
+    );
 };
 
 export default DashboardNavbar;

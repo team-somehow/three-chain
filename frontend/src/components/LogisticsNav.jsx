dfcb2453fd7d94bbc1d032afccbfc68cb2a77da1
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-04-16 05:03:08 +0530

    feat: map for logistics

--- frontend/src/components/LogisticsNav.jsx
+++ frontend/src/components/LogisticsNav.jsx
@@ -1,72 +1,80 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import NavLink from './NavLink';
+import React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import NavLink from "./NavLink";
 
-import ApartmentIcon from '@mui/icons-material/Apartment';
-import HandshakeIcon from '@mui/icons-material/Handshake';
-import ChatIcon from '@mui/icons-material/Chat';
-import AddIcon from '@mui/icons-material/Add';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import ChatIcon from "@mui/icons-material/Chat";
+import AddIcon from "@mui/icons-material/Add";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 
 const LogisticsNavbar = (props) => {
-  const navigate = useNavigate();
-  const currentRoute = useLocation().pathname;
-  return (
-    <Drawer
-      sx={{
-        width: 350,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: 280,
-          boxSizing: 'border-box',
-          // background: "rgba(252, 254, 254, 0.43)",
-          // background: "rgba(255, 0, 0, 0.85)",
-          // backdropFilter: "blur(25px)",
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-      classes={{ paper: 'awesome-bg-0' }}
-    >
-      <Box role="presentation" p={2}>
-        <List>
-          <div
-            style={{
-              paddingLeft: '18px',
-              marginTop: '-12px',
-              marginBottom: '28px',
-              cursor: 'pointer',
-              display: 'flex',
-              alignItems: 'center',
+    const navigate = useNavigate();
+    const currentRoute = useLocation().pathname;
+    return (
+        <Drawer
+            sx={{
+                width: 350,
+                flexShrink: 0,
+                "& .MuiDrawer-paper": {
+                    width: 280,
+                    boxSizing: "border-box",
+                    // background: "rgba(252, 254, 254, 0.43)",
+                    // background: "rgba(255, 0, 0, 0.85)",
+                    // backdropFilter: "blur(25px)",
+                },
             }}
-          >
-            <ListItem onClick={() => navigate('/logistics')}>
-              <ListItemText>
-                <h2>Dashboard</h2>
-              </ListItemText>
-            </ListItem>
-          </div>
+            variant="permanent"
+            anchor="left"
+            classes={{ paper: "awesome-bg-0" }}
+        >
+            <Box role="presentation" p={2}>
+                <List>
+                    <div
+                        style={{
+                            paddingLeft: "18px",
+                            marginTop: "-12px",
+                            marginBottom: "28px",
+                            cursor: "pointer",
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <ListItem onClick={() => navigate("/logistics")}>
+                            <ListItemText>
+                                <h2>Dashboard</h2>
+                            </ListItemText>
+                        </ListItem>
+                    </div>
 
-          <NavLink
-            text={'Get Details'}
-            icon={<HandshakeIcon />}
-            onClickNavigateTo="/logistics/details"
-            isActive={currentRoute === '/logistics/details'}
-          />
-        </List>
-        <Box
-          position={'absolute'}
-          width={'calc(100% - 20px)'}
-          bottom={'20px'}
-          margin={'auto'}
-        ></Box>
-      </Box>
-    </Drawer>
-  );
+                    <NavLink
+                        text={"Visualize"}
+                        icon={<RemoveRedEyeIcon />}
+                        onClickNavigateTo="/logistics"
+                        isActive={currentRoute === "/logistics"}
+                    />
+                    <NavLink
+                        text={"Get Details"}
+                        icon={<HandshakeIcon />}
+                        onClickNavigateTo="/logistics/details"
+                        isActive={currentRoute === "/logistics/details"}
+                    />
+                </List>
+                <Box
+                    position={"absolute"}
+                    width={"calc(100% - 20px)"}
+                    bottom={"20px"}
+                    margin={"auto"}
+                ></Box>
+            </Box>
+        </Drawer>
+    );
 };
 
 export default LogisticsNavbar;
--- frontend/src/screens/logistics/HomeLog.jsx
+++ frontend/src/screens/logistics/HomeLog.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React from "react";
+import { Map, Marker } from "react-map-gl";
 
 function HomeLog() {
-  return (
-    <div>Map</div>
-  )
+    return (
+        <div>
+            <Map
+                initialViewState={mapInitialViewState}
+                style={mapContainerStyles}
+                mapStyle="mapbox://styles/mbmph/clghxs0u2006401pkcfls6cog"
+                mapboxAccessToken={
+                    "pk.eyJ1IjoibWJtcGgiLCJhIjoiY2tya2F0OTJvMGk1YjJwbGZ1bDJ1eGU0dCJ9.fLJp01SsIpdhGmWdBzaSnQ"
+                }
+                renderWorldCopies={false}
+                onClick={() => {}}
+            >
+                {/* pin */}
+                <Marker
+                    latitude={19.100343}
+                    longitude={72.898441}
+                    style={markerStyle}
+                    onClick={() => {}}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/map/pin1.png"}
+                        width={30}
+                        alt="da"
+                    />
+                </Marker>
+
+                {/* pin */}
+                <Marker
+                    latitude={15.3173}
+                    longitude={75.7139}
+                    style={markerStyle}
+                    onClick={() => {}}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/map/pin2.png"}
+                        width={30}
+                        alt="da"
+                    />
+                </Marker>
+
+                {/* pin */}
+                <Marker
+                    latitude={19.8702388}
+                    longitude={75.2227995}
+                    style={markerStyle}
+                    onClick={() => {}}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/map/pin2.png"}
+                        width={30}
+                        alt="da"
+                    />
+                </Marker>
+
+                {/* pin */}
+                <Marker
+                    latitude={19.9662361}
+                    longitude={79.1931393}
+                    style={markerStyle}
+                    onClick={() => {}}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/map/pin3.png"}
+                        width={30}
+                        alt="da"
+                    />
+                </Marker>
+
+                {/* warehouse */}
+                <Marker
+                    latitude={17.3391522}
+                    longitude={76.7575381}
+                    style={markerStyle}
+                    onClick={() => {}}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/map/manu.png"}
+                        width={30}
+                        alt="da"
+                    />
+                </Marker>
+            </Map>
+        </div>
+    );
 }
 
-export default HomeLog
+// initial state on the map
+const mapInitialViewState = {
+    latitude: 20.5937,
+    longitude: 78.9629,
+    zoom: 5,
+};
+
+export const mapContainerStyles = {
+    height: "100vh",
+    width: "100vw",
+};
+
+const markerStyle = {
+    cursor: "pointer",
+};
+
+export default HomeLog;

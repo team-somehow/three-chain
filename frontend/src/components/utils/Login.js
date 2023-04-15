0be87a153134da40a7c4a3ababecdbd16d6f5a41
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-04-15 21:06:08 +0530

    feat: supplier onboarding

--- frontend/src/components/utils/Login.js
+++ frontend/src/components/utils/Login.js
@@ -4,36 +4,36 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = ({ onLoginNavigateTo, heading }) => {
-  const navigate = useNavigate();
-  const auth = useAuth();
+    const navigate = useNavigate();
+    const auth = useAuth();
 
-  useEffect(() => {
-    console.log("auth in Login.js", auth);
-    if (auth.isLoggedIn) {
-      navigate(onLoginNavigateTo);
-    }
-  }, [auth, navigate, onLoginNavigateTo]);
+    useEffect(() => {
+        console.log("auth in Login.js", auth);
+        if (auth.isLoggedIn) {
+            // navigate(onLoginNavigateTo);
+        }
+    }, [auth, navigate, onLoginNavigateTo]);
 
-  return (
-    <div className="bg min-h-screen">
-      <h1 className="text-center text-4xl p-8">{heading}</h1>
-      {auth.loading ? (
-        <div className="loading mt-40">
-          <CircularProgress />
+    return (
+        <div className="bg min-h-screen">
+            <h1 className="text-center text-4xl p-8">{heading}</h1>
+            {auth.loading ? (
+                <div className="loading mt-40">
+                    <CircularProgress />
+                </div>
+            ) : (
+                <div>
+                    <Auth
+                        externalWallet={false}
+                        theme="dark"
+                        onLogin={() => {
+                            navigate(onLoginNavigateTo);
+                        }}
+                    ></Auth>
+                </div>
+            )}
         </div>
-      ) : (
-        <div>
-          <Auth
-            externalWallet={false}
-            theme="dark"
-            onLogin={() => {
-              navigate(onLoginNavigateTo);
-            }}
-          ></Auth>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Login;

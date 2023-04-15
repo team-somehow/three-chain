d5cdc9ee2c26af294c0fea0246acaa3a258bae12
Vinay <vinaykanse9451@gmail.com>
2023-04-16 00:40:54 +0530

    fix: solve merge issues and minor fixes

--- frontend/src/screens/supplier/loan.js
+++ frontend/src/screens/supplier/loan.js
@@ -12,7 +12,7 @@ import { LoanContractAddress } from "../../constants/constants";
 import LoanAbi from "../../artifacts/contracts/Loan.sol/Loan.json";
 import CustomCard from "../../components/CustomCard";
 import CustomButton from "../../components/CustomButton";
-import CurrencyRupeeRoundedIcon from '@mui/icons-material/CurrencyRupeeRounded';
+import CurrencyRupeeRoundedIcon from "@mui/icons-material/CurrencyRupeeRounded";
 import CustomAppBar from "../../components/AppBar";
 
 function Loan() {
@@ -55,42 +55,50 @@ function Loan() {
         setTotalAmount(A);
     }, [loanAmount, interest, selectedDuration]);
 
-  return (
-    <>
-    < Typography variant="h4" textAlign='center' fontWeight='600' sx={{marginTop : '1rem'}}>
-      Request Loan
-    </Typography>
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "90vh",
-        p: 2,
-      }}
-    >
-      <CustomCard >
-        <Typography mt={2} variant="h4" fontWeight='500' textAlign='center'>
-          Loan Details
-        </Typography>
-        <form style={{marginTop : '1rem'}}>
-          <RuppeInput input={loanAmount} setInput={setLoanAmount} />
-          <Dropdown
-            label={"Duration(in Months) "}
-            options={durationOptions}
-            inputValue={selectedDuration}
-            setInputValue={setSelectedDuration}
-          />
-          <Divider
-            sx={{
-              my: 3,
-            }}
-          />
-          <Typography variant="h5" fontWeight='600'>Interest: {interest}%</Typography>
-          <Typography variant="h5" fontWeight='600'>
-            Total Amount: {totalAmount.toFixed(2)}
-          </Typography>
-          {/* <Button
+    return (
+        <>
+            <CustomAppBar title={"Request Loan"} onBackUrl={"/supplier/home"} />
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "90vh",
+                    p: 2,
+                }}
+            >
+                <CustomCard>
+                    <Typography
+                        mt={2}
+                        variant="h4"
+                        fontWeight="500"
+                        textAlign="center"
+                    >
+                        Loan Details
+                    </Typography>
+                    <form style={{ marginTop: "1rem" }}>
+                        <RuppeInput
+                            input={loanAmount}
+                            setInput={setLoanAmount}
+                        />
+                        <Dropdown
+                            label={"Duration(in Months) "}
+                            options={durationOptions}
+                            inputValue={selectedDuration}
+                            setInputValue={setSelectedDuration}
+                        />
+                        <Divider
+                            sx={{
+                                my: 3,
+                            }}
+                        />
+                        <Typography variant="h5" fontWeight="600">
+                            Interest: {interest}%
+                        </Typography>
+                        <Typography variant="h5" fontWeight="600">
+                            Total Amount: {totalAmount.toFixed(2)}
+                        </Typography>
+                        {/* <Button
           variant="contained"
           sx={{
             mt: 4,
@@ -103,7 +111,7 @@ function Loan() {
                             sx={{
                                 mt: 2,
                                 mb: 1,
-              display: "flex",
+                                display: "flex",
                                 justifyContent: "center",
                                 alignItems: "center",
                             }}
@@ -111,15 +119,18 @@ function Loan() {
                             <CustomButton
                                 text={"Request Manufacturer"}
                                 onPress={() => RequestLoan()}
-                              icon={<CurrencyRupeeRoundedIcon sx={{marginRight: '0.5rem'}} />}
-            />
+                                icon={
+                                    <CurrencyRupeeRoundedIcon
+                                        sx={{ marginRight: "0.5rem" }}
+                                    />
+                                }
+                            />
                         </Box>
                     </form>
                 </CustomCard>
             </Box>
         </>
-      </>
-  );
+    );
 }
 
 export default Loan;

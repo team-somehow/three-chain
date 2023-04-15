224a6f2511298487e7e6afc0734119472b34f620
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-04-15 22:56:41 +0530

    feat: buyer bid

--- frontend/src/screens/buyer/BuyerBid.js
+++ frontend/src/screens/buyer/BuyerBid.js
@@ -6,34 +6,42 @@ import BuyerBiddingItem from "../../components/buyer/BuyerBiddingItem";
 import { db } from "../../config/firebase";
 
 const BuyerBid = () => {
-	const [data, setData] = useState([]);
-	useEffect(() => {
-		const getData = async () => {
-			let tData = [];
-			const snapshot = await getDocs(collection(db, "Manufacturer"));
-			snapshot.forEach((doc) => {
-				const t = doc.data();
-				for (let i = 0; i < t.products.length; i++) {
-					if (t.products[i].regulatorVerification) {
-						tData.push(t.products[i]);
-					}
-				}
-			});
-			setData(tData);
-		};
-		getData();
-	}, []);
+    const auth = useAuth();
 
-	return (
-		<Box>
-			<Typography variant="h3" align="center">
-				Bid for Items
-			</Typography>
-			{data.map((item) => {
-				return <BuyerBiddingItem {...item} />;
-			})}
-		</Box>
-	);
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            let tData = [];
+            const snapshot = await getDocs(collection(db, "Manufacturer"));
+            snapshot.forEach((doc) => {
+                const t = doc.data();
+                for (let i = 0; i < t.products.length; i++) {
+                    if (t.products[i].regulatorVerification) {
+                        let obj = t.products[i].bids.find(
+                            (o) => o.walletAddress === auth.user.address
+                        );
+
+                        if (obj) {
+                            tData.push(t.products[i]);
+                        }
+                    }
+                }
+            });
+            setData(tData);
+        };
+        getData();
+    }, []);
+
+    return (
+        <Box>
+            <Typography variant="h3" align="center">
+                Bid for Items
+            </Typography>
+            {data.map((item) => {
+                return <BuyerBiddingItem {...item} />;
+            })}
+        </Box>
+    );
 };
 
 export default BuyerBid;
